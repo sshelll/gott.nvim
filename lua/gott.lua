903f22794510c7f83d0ca33871c42d22ab1d9a32
sshelll <shaojiale.gitignore@icloud.com>
2023-07-21 22:58:44 +0800

    add license

--- lua/gott.lua
+++ lua/gott.lua
@@ -1,3 +1,7 @@
+-- Copyright (c) 2023 sshelll, the gott.nvim authors. All rights reserved.
+-- Use of this source code is governed by a BSD-style license that can
+-- be found in the LICENSE file.
+
 local gott = {}
 local builtin = require('gott_builtin')
 
